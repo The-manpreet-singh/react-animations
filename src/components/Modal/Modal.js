897be228e0125ea7e4d458fee5e082035f32f21c
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-16 23:05:55 +0530

    customizing css

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -15,7 +15,12 @@ const modal = (props) => {
         unmountOnExit
          in={props.show} 
          timeout={animationTiming}
-         classNames='fade-slide' >
+         classNames={{
+             enter: '',
+             enterActive: 'ModalOpen',
+             exit: '',
+             exitActive: 'ModalClosed'
+         }} >
                 <div className="Modal">
                 <h1>A Modal</h1>
                 <button className="Button" onClick={props.closed}>Dismiss</button>
