abde1f796e4de206f79870b92f8e8ed66615339a
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-16 12:49:45 +0530

    add backdrop and model

--- src/App.js
+++ src/App.js
@@ -6,13 +6,29 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
+
+  state= {
+    modalIsOpen: false
+  }
+
+  showModel = () => {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModel = () => {
+    this.setState({modalIsOpen: false});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <Modal 
+        show={this.state.modalIsOpen}
+        closed={this.closeModel}
+        />
+        <Backdrop show={this.state.modalIsOpen} />
+        <button className="Button" onClick={this.showModel}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = (props) => {
+    const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed'];
+
+    return <div className={cssClasses.join(' ')}></div>;
+};
 
 export default backdrop;
